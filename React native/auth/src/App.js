d66d96010841e3c2f35d80948d76d2a2cbe60ada
Catherine Chen <yacatherine19@gmail.com>
2018-09-05 00:04:46 +0800

    [RN] imported firebase

--- React native/auth/src/App.js
+++ React native/auth/src/App.js
@@ -8,14 +8,30 @@
 
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
-export default class App extends Component {
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC-tVpwP2NaGDF5IukKbPL6fBc_Fr188ug',
+      authDomain: 'auth-1579f.firebaseapp.com',
+      databaseURL: 'https://auth-1579f.firebaseio.com',
+      projectId: 'auth-1579f',
+      storageBucket: 'auth-1579f.appspot.com',
+      messagingSenderId: '335086132063'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
 }
+
+export default App;
---
+++ React native/auth/src/components/LoginForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Button } from './common';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <Card>
+                <CardSection />
+                <CardSection />
+
+                <CardSection>
+                    <Button>Log in</Button>
+                </CardSection>
+            </Card>
+         );
+    }
+}
+
+export default LoginForm;
