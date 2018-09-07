fad977dd6ba06ca3ab3657b943ee48b24f3081e1
Catherine Chen <catherine_chen@trendmicro.com>
2018-09-07 18:25:43 +0800

    [RN] added redux

--- React native/tech_stack/src/reducers/Index.js
+++ React native/tech_stack/src/reducers/Index.js
@@ -1,7 +1,8 @@
 // We wanna have multiple reducers like multiple different pieces of state
 // To make always different pieces of reducers play nicely together, we use combineReducers
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-    libraries: () => []
+  libraries: LibraryReducer
 });
