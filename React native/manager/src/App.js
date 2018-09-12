32a4316269b8640a129772fe51d6b7008e6b18cd
Catherine Chen <catherine_chen@trendmicro.com>
2018-09-12 10:52:00 +0800

    [RN] Import firebase

--- React native/manager/src/App.js
+++ React native/manager/src/App.js
@@ -10,17 +10,33 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
-  render() {
-     // Post reducers to the createStore called
-    return (
-        <Provider store={createStore(reducers)}>
-            <View />
-        </Provider>
-    );
-  }
+
+    componentWillMount() {
+        // Install Firebase with 'npm install --save firebase' command
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyAJ42gwnaaUqvxqnhVL_MYUYq3gbB51glo',
+            authDomain: 'manager-9c412.firebaseapp.com',
+            databaseURL: 'https://manager-9c412.firebaseio.com',
+            projectId: 'manager-9c412',
+            storageBucket: 'manager-9c412.appspot.com',
+            messagingSenderId: '696437076833'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        // Post reducers to where the createStore called
+        return (
+            <Provider store={createStore(reducers)}>
+                <View />
+            </Provider>
+        );
+    }
 }
 
 export default App;
