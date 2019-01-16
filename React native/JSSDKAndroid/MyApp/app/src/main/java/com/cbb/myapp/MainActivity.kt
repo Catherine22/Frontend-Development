c71e92cf308bdc7cddb4a5949efece8a5116f868
Catherine Chen <catherine_chen@trendmicro.com>
2019-01-16 13:00:43 +0800

    [iOS] removed duplicated embedded binaries [Android] added SearchView (https://github.com/Mauker1/MaterialSearchView)

--- React native/JSSDKAndroid/MyApp/app/src/main/java/com/cbb/myapp/MainActivity.kt
+++ React native/JSSDKAndroid/MyApp/app/src/main/java/com/cbb/myapp/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.cbb.myapp
 
-import android.app.SearchManager
+import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import br.com.mauker.materialsearchview.MaterialSearchView
+import android.text.TextUtils
+import android.speech.RecognizerIntent
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+    private var searchView: MaterialSearchView? = null
+    private var tv_log: TextView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +28,107 @@ class MainActivity : AppCompatActivity() {
         initComponent()
     }
 
-    private fun initComponent() {
-        // Verify the action and get the query
-        if (Intent.ACTION_SEARCH == intent.action) {
-            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
-                queryData(query)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if (searchView?.isOpen!!) {
+            // Close the search on the back button press.
+            searchView?.closeSearch()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        // Handle toolbar item clicks here. It'll
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item?.itemId
+
+        when (id) {
+            R.id.action_search -> {
+                // Open the search view on the menu item click.
+
+                searchView?.openSearch()
+                return true
             }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == Activity.RESULT_OK) {
+            val matches = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (matches != null && matches.size > 0) {
+                val searchWrd = matches[0]
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView?.setQuery(searchWrd, false)
+                }
+            }
+
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        searchView?.clearSuggestions()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        searchView?.activityResumed()
+        val arr = resources.getStringArray(R.array.suggestions)
+
+        searchView?.addSuggestions(arr)
+    }
+
+
+    private fun initComponent() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        tv_log = findViewById(R.id.tv_log)
+        searchView = findViewById(R.id.search_view)
+        searchView?.adjustTintAlpha(0.8f)
+        searchView?.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                queryData(query)
+                searchView?.clearAll()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        searchView?.setSearchViewListener(object : MaterialSearchView.SearchViewListener {
+            override fun onSearchViewClosed() {
+                // Do something once the view is closed.
+            }
+
+            override fun onSearchViewOpened() {
+                // Do something once the view is open.
+            }
+
+        })
+        searchView?.setOnItemClickListener { _, _, position, _ ->
+            val suggestion = searchView?.getSuggestionAtPosition(position)
+            searchView?.setQuery(suggestion, false)
+        }
+        searchView?.setOnVoiceClickedListener {
+            Toast.makeText(this@MainActivity, "Voice clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 
     private fun queryData(query: String) {
-        Log.d(TAG, String.format("Query: %s", query))
+        tv_log?.text = String.format("Query: %s", query)
     }
 }
+
