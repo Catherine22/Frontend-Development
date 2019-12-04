c8d1d8011a35f93293197d6fbd696e5c2eaa384a
Catherine <yacatherine19@gmail.com>
2019-12-04 09:33:50 +0800

    added Vue app

---
+++ Vue/README.md
@@ -0,0 +1,7 @@
+# Vue.js
+
+## Prerequisites
+1. Chrome		
+2. Node.js		
+3. Vue cli		
+4. Vue.js devtools (chrome extension)
---
+++ Vue/buying-socks-website/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'assets/vmSocks-green-onWhite.jpg'
+    }
+});
