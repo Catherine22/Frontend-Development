274aea00f646d11fa42c77f754a22c3951a400eb
Catherine <yacatherine19@gmail.com>
2019-12-04 12:05:48 +0800

    added v-bind, v-if/else and v-for for list

--- Vue/buying-socks-website/main.js
+++ Vue/buying-socks-website/main.js
@@ -2,6 +2,22 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: 'assets/vmSocks-green-onWhite.jpg'
+        imageAlt: 'A pair of socks',
+        imageGreenSocks: 'assets/vmSocks-green-onWhite.jpg',
+        imageBlueSocks:
+            'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+        inStock: false,
+        inventory: 9,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColour: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColour: 'blue'
+            }
+        ]
     }
 });
