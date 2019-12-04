ee5b766ed1d6924c12f3ccdf5f27400354dddd9d
Catherine <yacatherine19@gmail.com>
2019-12-04 14:07:12 +0800

    added style bindings

--- Vue/buying-socks-website/main.js
+++ Vue/buying-socks-website/main.js
@@ -2,22 +2,32 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
+        image: 'assets/vmSocks-green-onWhite.jpg',
         imageAlt: 'A pair of socks',
-        imageGreenSocks: 'assets/vmSocks-green-onWhite.jpg',
-        imageBlueSocks:
-            'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
-        inStock: false,
+        inStock: true,
         inventory: 9,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColour: 'green'
+                variantColour: 'green',
+                variantImage: 'assets/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColour: 'blue'
+                variantColour: 'blue',
+                variantImage:
+                    'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProductImage(image) {
+            this.image = image;
+        }
     }
 });
