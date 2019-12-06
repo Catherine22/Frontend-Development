9ccefb82398ff449487d6efe9cc0b44b7d80d472
Catherine <yacatherine19@gmail.com>
2019-12-06 10:56:30 +0800

    set eslint

---
+++ Vue/vue-essentials/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        semi: [1, 'always'],
+        camelcase: 2
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
+        }
+    ]
+};
